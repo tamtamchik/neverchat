7f81791a5c83af1865a6eab0377bbfdc732a12e7
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2014-05-20 16:46:15 +0400

    added Base64 to code messages

--- app/src/requireConfig.js
+++ app/src/requireConfig.js
@@ -8,7 +8,8 @@ require.config({
         requirejs: '../lib/requirejs/require',
         almond: '../lib/almond/almond',
         'famous-polyfills': '../lib/famous-polyfills/index',
-        showdown: '../lib/showdown/src/showdown'
+        showdown: '../lib/showdown/src/showdown',
+        'js-base64': '../lib/js-base64/base64'
     }
 });
 require(['App']);
