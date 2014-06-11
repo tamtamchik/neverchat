9747595f1d70b163fc1e994b86aad0847a3ebc3d
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2014-06-11 12:34:42 +0400

    avatars added, scroll is scrolling

--- app/src/views/MessageView.js
+++ app/src/views/MessageView.js
@@ -7,6 +7,7 @@ define(function(require, exports, module) {
     var Transform          = require('famous/core/Transform');
     var StateModifier      = require('famous/modifiers/StateModifier');
     var ImageSurface       = require('famous/surfaces/ImageSurface');
+    var ContainerSurface   = require('famous/surfaces/ContainerSurface');
 
     // =================================================================================================================
     function MessageView(msg) {                                               // Constructor function for MessageView class
@@ -14,7 +15,8 @@ define(function(require, exports, module) {
         // Applies View's constructor function to MessageView class
         View.apply(this, arguments);
 
-        _createAvatar.apply(this);
+        _createBaseSurface.call(this);
+        _createAvatar.call(this);
     }
 
     // Establishes prototype chain for MessageView class to inherit from View
@@ -24,25 +26,48 @@ define(function(require, exports, module) {
     // =================================================================================================================
     MessageView.DEFAULT_OPTIONS = {                                             // Default options for MessageView class
         avatarHeight: 50,
-        avatarWidth: 50,
+        avatarProperties: {
+            border: '3px solid rgba(255, 255, 255, 0.33)',
+            borderRadius: '999em'
+        },
         avatarUrl: {
             prefix: 'http://www.gravatar.com/avatar/',
             postfix: '?s=200&d=identicon'
         },
+        avatarWidth: 50,
+        messageMinHeight: 100
     };
 
     // =================================================================================================================
                                                                                                        // Layout section
 
     // -----------------------------------------------------------------------------------------------------------------
+    function _createBaseSurface() {                                                      // Creates base message surface
+        this.container = new ContainerSurface();
+
+        var containerModifier = new StateModifier();
+
+        containerModifier.setSize([undefined, this.options.messageMinHeight]);
+
+        this.container.pipe(this._eventOutput);
+        this.add(containerModifier).add(this.container);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
     function _createAvatar() {                                                                      // Avatar generation
         var avatar = new ImageSurface({
+            classes: ['no-selection'],
             size: [this.options.avatarHeight, this.options.avatarWidth],
-            content: this.options.avatarUrl.prefix + this.message.content.user + this.options.avatarUrl.postfix
-        })
+            content: this.options.avatarUrl.prefix + this.message.content.user + this.options.avatarUrl.postfix,
+            properties: this.options.avatarProperties
+        });
 
-        avatar.pipe(this);
-        this.add(avatar);
+        var avatarModirier = new StateModifier({
+            transform: Transform.translate(10, 10, 0.1)
+        });
+
+        avatar.pipe(this.container);
+        this.container.add(avatarModirier).add(avatar);
     }
 
     // =================================================================================================================
